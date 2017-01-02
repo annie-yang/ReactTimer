b6df3e02c2f6222a4f7b0c50282d1ff737d7e2b1
Annie Y <annie_y48@hotmail.com>
2017-01-01 16:10:52 -0800

    add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,8 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 //components
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // load foundation
 // 'css!' is a css loader
@@ -23,6 +25,8 @@ ReactDOM.render(
 	// 'IndexRoute' if the path doesn't recognize other links, then go to the index route path within the 'Main' component
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
+			<Route path="countdown" component={Countdown}></Route>
+			<IndexRoute component={Timer}/>
 		</Route>
 	</Router>,
 	document.getElementById('app')
