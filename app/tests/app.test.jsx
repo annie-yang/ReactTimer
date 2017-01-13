01a5628308444bde730c5d019d54ec24b505718d
Annie Y <annie_y48@hotmail.com>
2017-01-13 14:47:22 -0800

    Added comments

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -2,8 +2,11 @@ var expect = require('expect');
 
 // formats output to be more readable
 describe('App', () => {
-  // describes what the function should do
+  /*
+    describes what the test should do
+    first takes string, the second takes assertions
+  */
   it('should properly run tests', () => {
-    expect(1).toBe(1);
+    expect(1).toBe(1); // expect one to be equal to one
   });
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,37 @@
+// adding configs for testing purposes only
+
 var webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config){
   config.set({ // set configurations
     browsers: ['Chrome'], // test our debugs in Chrome browser
     singleRun: true,
-    frameworks: ['mocha'],
+    frameworks: ['mocha'], // use mocha framework that has 'describe' and 'it'
 
-    // globing pattern - basic file pattern
-    // get tests file in tests folder ** (or in any sub directories) that has a file name of star of anything that end in of 'test.jsx'
+    /*
+      globing pattern - basic file pattern to match the tests
+    */
     files: [
       'node_modules/jquery/dist/jquery.min.js',
       'node_modules/foundation-sites/dist/foundation.min.js',
       './app/app.jsx',
-      'app/tests/**/*.test.jsx'
+      'app/tests/**/*.test.jsx' // get tests file in tests folder ** (or in any sub directories) that has a file name of star of anything, but ends in of 'test.jsx'
     ], // test files to be executed
 
-    // specifying the things we want to do in our test files
+    // specifying the things we want to do with our test files
     preprocessors: {
       // run 'webpack' and 'sourcemap' on the test files
       'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
     },
     // which tests passed/failed
-    reporters: ['mocha'],
+    reporters: ['mocha'], // shows the checkmarks
     // tests timeout (if website doesn't load within certain amount of seconds , timeout the page)
     client:{
       mocha:{
         timeout: '5000' // 5 seconds
       }
     },
-    webpack: webpackConfig,
+    webpack: webpackConfig, // load in webpack config
     webpackServer: {
       noInfo: true
     }
